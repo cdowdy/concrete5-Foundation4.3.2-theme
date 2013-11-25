2d79c12f4b32c86a48d0a644327807c329356eaa
cdowdy <cdowdy46@gmail.com>
2013-11-25 08:37:37 -0500

    header and footer

---
+++ themes/Foundation-4-3-2/elements/footer.php
@@ -0,0 +1,28 @@
+<?php 
+defined('C5_EXECUTE') or die('Access_Denied');
+?>
+<!-- Footer -->
+
+  <footer class="row">
+    <div class="large-12 columns">
+      <hr />
+      <div class="row">
+        <div class="large-6 columns">
+          <p>&copy; <?php print date('Y') . ' ' . SITE; ?></p>
+        </div>
+        <div class="large-6 columns">
+          <?php
+	     	$a = new GlobalArea('Footer Nav');
+	     	$a->display($c);
+	     ?>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <script>
+    $(document).foundation();
+  </script>
+ <script src="<?php echo $this->getThemePath(); ?>/js/foundation.min.js"></script>
+ <?php Loader::element('footer_required'); ?>
+</html>
